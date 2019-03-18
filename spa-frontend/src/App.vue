e410e03865a6feb6c3f945381ed04be3df0b2848
masahiro hosoya <hosoya1985@gmail.com>
2019-03-18 23:09:51 +0900

    add header & footer

--- spa-frontend/src/App.vue
+++ spa-frontend/src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <app-header></app-header>
+    <router-view/>
+    <app-footer></app-footer>
   </div>
 </template>
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
+export default {
+  components: {
+    AppHeader,
+    AppFooter
+  }
+}
+</script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ spa-frontend/src/components/AppFooter.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        <strong>serverless</strong>
+        <router-link to="/about">about</router-link>
+      </p>
+    </div>
+  </footer>
+</template>
